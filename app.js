b842b6dfc8254ec3c26d144188231775c3a74dc6
Tom Beadie <>
2024-11-11 21:17:41 -0500

    Show hidden element message container on click

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(";jkafd");
+const ratingCircleOne = document.getElementById("circ1");
+const ratingCircleTwo = document.getElementById("circ2");
+const ratingCircleThree = document.getElementById("circ3");
+const ratingCircleFour = document.getElementById("circ4");
+const ratingCircleFive = document.getElementById("circ5");
+
+const msgContainer = document.querySelector(".message-container");
+
+
+
+ratingCircleOne.addEventListener("click", (e) => {
+  console.log("ayy");
+    msgContainer.style.display = 'block'
+});
